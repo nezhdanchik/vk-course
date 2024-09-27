9113e454d44b88276ec2c6f42b04f975f819d4ba
nezhdanchik <kirdanchik@yandex.ru>
2024-09-27 11:46:29 +0300

    add fileManager to ile_filter.py

--- 01/file_filter.py
+++ 01/file_filter.py
@@ -1,12 +1,31 @@
+class FileManager:
+    def __init__(self, file):
+        self.file = file
+        self.opened_file = None
+
+    def __enter__(self):
+        # передан путь к файлу
+        if isinstance(self.file, str):
+            self.opened_file = open(self.file, encoding='utf-8')
+            return self.opened_file
+        # передан файловый объект
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self.opened_file:
+            self.opened_file.close()
+
+
 def file_filter(file, find_list, stop_list):
-    find_list = set(find_list)
-    stop_list = set(stop_list)
+    find_list = {i.lower() for i in find_list}
+    stop_list = {i.lower() for i in stop_list}
 
-    for line in file:
-        words = set(line.strip().lower().split())
+    with FileManager(file) as f:
+        for line in f:
+            words = set(line.strip().lower().split())
 
-        if words & stop_list:
-            continue
+            if words & stop_list:
+                continue
 
-        if words & find_list:
-            yield line.strip()
+            if words & find_list:
+                yield line.strip()
