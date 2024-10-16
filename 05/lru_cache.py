4ff34d86ddb9bca0a5863d771f23f6344b7985ee
nezhdanchik <kirdanchik@yandex.ru>
2024-10-16 19:29:10 +0300

    start task5

---
+++ 05/lru_cache.py
@@ -0,0 +1,43 @@
+from collections import abc
+
+class MyDict(dict):
+    def move_elem_to_end(self, key):
+        val = self[key]
+        del self[key]
+        self[key] = val
+
+    def remove_first_elem(self):
+        if len(self) > 0:
+            del self[next(iter(self))]
+        else:
+            raise Exception('dict is empty, can\'t remove first elem')
+
+# LRU - Last Recently Used
+class LRUCache:
+    def __init__(self, limit=42):
+        if limit <= 0:
+            raise ValueError('Limit must be > 0')
+        self.limit = limit
+        self._data = MyDict()
+
+    def get_data_keys(self):
+        return tuple(self._data.keys())
+
+    def get(self, key):
+        if not isinstance(key, abc.Hashable):
+            raise TypeError('Key must be hashable')
+        if key not in self._data:
+            return None
+        self._data.move_elem_to_end(key)
+        return self._data[key]
+
+    def set(self, key, value):
+        if len(self._data) == self.limit:
+            self._data.remove_first_elem()
+        self._data[key] = value
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
---
+++ 05/test_lru_cache.py
@@ -0,0 +1,95 @@
+from functools import cache
+from random import random
+from time import perf_counter
+import numpy as np
+import pytest
+
+from lru_cache import LRUCache
+
+
+def test_before_limit():
+    cache = LRUCache(2)
+    cache.set("k1", "val1")
+    cache["k2"] = "val2"
+    assert cache.get('k1') == 'val1'
+    assert cache['k2'] == 'val2'
+
+
+def test_exceed_limit():
+    cache = LRUCache(2)
+
+    cache.set("k1", "val1")
+    cache.set("k2", "val2")
+
+    _, __ = cache['k2'], cache['k1']
+
+    cache.set("k3", "val3")
+
+    assert cache.get("k3") == "val3"
+    assert cache.get("k2") is None
+    assert cache.get("k1") == "val1"
+
+def test_get():
+    cache = LRUCache(3)
+    assert cache.get("k1") is None
+    cache.set("k1", "val1")
+    assert cache.get("k1") == "val1"
+    cache["k2"] = "val2"
+    assert cache["k2"] == "val2"
+
+
+def test_set():
+    cache = LRUCache(3)
+    cache["k1"] = "val1"
+    cache['k1'] = 'val2'
+    assert cache['k1'] == 'val2'
+
+    with pytest.raises(TypeError):
+        cache[{1: 2}] = 'val3'
+
+def test_bad_limit():
+    with pytest.raises(ValueError):
+        cache = LRUCache(0)
+    with pytest.raises(ValueError):
+        cache = LRUCache(-10)
+
+
+def test_not_existing_key():
+    cache = LRUCache()
+    assert cache[1] is None
+
+
+def stress_time(quantity, limit=100):
+    start = perf_counter()
+    cache = LRUCache(limit=limit)
+    # set
+    for _ in range(quantity):
+        cache.set(random(), random())
+
+    # get
+    for key in cache.get_data_keys():
+        cache.get(key)
+
+    end = perf_counter()
+    return end - start
+
+
+def test_stress_time():
+    '''
+    Если операции set и get выполняются за приблизительно
+    O(1), то выполнение n раз подряд этих операций должно занимать
+    примерно O(n).
+    '''
+    quantities = list(np.linspace(10**3, 10**6, 20, dtype=int))
+    times = [stress_time(quantity) for quantity in quantities]
+    print()
+    rates = []
+    for i in range(len(quantities)):
+        rates.append(quantities[i] / times[i])
+
+    # стандартное отклонение отклоняется не более чем на 15% от среднего
+    mean_rate = np.mean(rates)
+    std_rate = np.std(rates)
+    threshold = 0.15 * mean_rate
+    print(f'{mean_rate=}, {std_rate=}, {threshold=}')
+    assert std_rate < threshold
